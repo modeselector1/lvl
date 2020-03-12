bb3a15a3776e3be23de86b5e10e521e217970ee3
modeselector1 <ivanova.maria.98@mail.ru>
2020-03-12 20:16:18 +0300

    feat: add java script

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
-  console.log(modal);
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]')
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
 });
