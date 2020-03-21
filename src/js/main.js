bbd8edb397291f22f09013c5f6258e20de832cd7
modeselector1 <ivanova.maria.98@mail.ru>
2020-03-21 09:57:21 +0300

    feat: add yandex map

--- src/js/main.js
+++ src/js/main.js
@@ -76,8 +76,41 @@ $(document).ready(function () {
     }
   });
     
-  // маскадля телефона
+  // маска для телефона
 
   $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
 
+  // создание yandex карты
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [47.244729, 39.723187],
+            zoom: 18,
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Наш офис',
+            balloonContent: 'Вход со двора'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/rating.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+   });    
 });
