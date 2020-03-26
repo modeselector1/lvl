2dabfd023e356221f032abac566cb6911808cd6b
modeselector1 <ivanova.maria.98@mail.ru>
2020-03-26 22:08:35 +0300

    add

---
+++ README.md
@@ -0,0 +1,3 @@
+# repair-design
+Сайт компании по ремонту квартир
+https://serhiikorniichuk.github.io/repair-design/src/
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
 
 function bs() {
   serveSass();
@@ -17,13 +20,56 @@ function bs() {
 };
 
 function serveSass() {
-  return src('./src/sass/**/*.sass', './src/sass/**/*.sсss')
+  return src('./src/sass/**/*.sass', './src/sass/**/*.scss')
     .pipe(sass())
     .pipe(autoprefixer({
         cascade: false
     }))
     .pipe(dest('./src/css'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 };
 
+function buildCSS(done) {
+  src('src/css/**/**.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(dest('dist/css/'));
+  done();
+}
+
+function buildJS(done) {
+  src('src/js/**.js')
+    .pipe(minify({
+        ext:{
+          src:'-debug.js',
+          min:'.js'
+        },
+        ignoreFiles: ['*.min.js']
+    }))
+    .pipe(dest('dist/js/'));
+  done();
+}
+
+function html(done) {
+  src('src/**.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist/'));
+
+  done();
+}
+
+function php(done) {
+  src('src/**.php')
+    .pipe(dest('dist/'));
+  src('src/phpmailer/**/**')
+    .pipe(dest('dist/phpmailer/'));
+  done();
+}
+
+function fonts(done) {
+  src('src/fonts/**/**')
+    .pipe(dest('dist/fonts/'));
+  done();
+}
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, fonts);
